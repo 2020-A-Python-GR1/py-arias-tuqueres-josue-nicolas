ea15c3d1e5e6abad1af94439687ffc47b72594ae
Nicolás Arias <josue.arias@epn.edu.ec>
2020-07-14 08:59:12 -0500

    Spyder y pandas

---
+++ 03 - pandas/a_introduccion.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 14 07:43:22 2020
+
+@author: Nicolas
+"""
+
+
+print("Hola spider")
+
+nombre = "Nicolas"
+numero = 3
+lista = [1,2,3]
+tupla = (1,2,3)
+diccionario = {
+    "nombre": "Nicolas",
+    "apellido": "Arias"
+    }
