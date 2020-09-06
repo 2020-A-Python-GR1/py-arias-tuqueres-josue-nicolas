1f95f73eb24e3df945c5672a5e9cca96df887f80
Nicolás Arias <josue.arias@epn.edu.ec>
2020-09-06 01:18:20 -0500

    Scrapy a .csv y scrapy.items

--- 04 - scrapy/03 - intro-spider/arania_basica/arania_basica/spiders/arania_ejemplo.py
+++ 04 - scrapy/03 - intro-spider/arania_basica/arania_basica/spiders/arania_ejemplo.py
@@ -2,16 +2,31 @@ import scrapy
 
 class IntroSpider(scrapy.Spider):
     name = "introduccion_spider"
-    urls = ['http://books.toscrape.com/catalogue/category/books/travel_2/index.html']
+    #urls = ['http://books.toscrape.com/catalogue/category/books/travel_2/index.html']
+    urls = ['https://www.fybeca.com/FybecaWeb/pages/search-results.jsf?cat=446&s=0&pp=3000']
 
     def start_requests(self):
         for url in self.urls:
             yield scrapy.Request(url=url)
 
     def parse(self, response):
+        
+        """
         etiqueta_contenedora = response.css(
             'article.product_pod'
         )
+        """
+        lista_programas = response.css(
+           'div.product-tile-inner > a.name::text' 
+        ).extract()
+
+        print("lista_titulos")
+        print(lista_programas)
+
+        print("Size lista_titulos")
+        print(len(lista_programas))
+
+        """
         titulos = etiqueta_contenedora.css(
             'h3 >a::text'
         ).extract()
@@ -23,11 +38,7 @@ class IntroSpider(scrapy.Spider):
             'div.product_price > p.availability::text'
             ).extract()
 
-        """
-        stock = etiqueta_contenedora.css(
-            'div.product_price > p.availability::attr(class)'
-            ).extract()
-        """
+
 
         starts = response.css(
             'article.product_pod>p::attr(class)'
@@ -48,3 +59,4 @@ class IntroSpider(scrapy.Spider):
         print("imagen",imagen)
 
 
+        """
